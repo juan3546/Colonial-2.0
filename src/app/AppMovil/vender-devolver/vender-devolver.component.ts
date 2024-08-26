112de18fc1f18e53aea85c82afe538481547c6c1
AllanParra765 <allan.parrae@gmail.com>
2024-08-25 18:32:24 -0600

    validar cambios de alerts y modales

--- src/app/AppMovil/vender-devolver/vender-devolver.component.ts
+++ src/app/AppMovil/vender-devolver/vender-devolver.component.ts
@@ -9,7 +9,12 @@ export class VenderDevolverComponent implements OnInit {
 
   constructor() { }
 
+  today: string;
+
   ngOnInit(): void {
-  } 
+//fecha de hoy para venta
+    const currentDate = new Date();
+    this.today = currentDate.toISOString().split('T')[0]; // Formato: YYYY-MM-DD
+  }
 
 }
